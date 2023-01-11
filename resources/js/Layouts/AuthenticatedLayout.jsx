1b0fc7b2e9eef68c043ac052cff6e593d7f4798f
vono04 <vono04@freemail.hu>
2023-01-11 22:06:14 +0100

    Transport route

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('transport')} active={route().current('transport')}>
+                                    Transport No.
+                                </NavLink>
                             </div>
                         </div>
 
@@ -94,6 +97,9 @@ export default function Authenticated({ auth, header, children }) {
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('transport')} active={route().current('transport')}>
+                            Transport No.
                         </ResponsiveNavLink>
                     </div>
 
